cd32ccd26bd12ce190caed07c5d18eced48cb3b9
Christian Tegnér <christian.tegner@gmail.com>
2014-04-07 12:58:25 +0200

    Add nomnom to parse CLI args

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 
-// TODO: Parse args
+var args = require("nomnom")
+    .option('port', {
+        abbr: 'p',
+        default: 0,
+        help: 'The server\'s port number'
+    })
+    .option('version', {
+        flag: true,
+        help: 'Print version and exit',
+        callback: function() {
+            return require('../package.json').version;
+        }
+    })
+    .parse();
 
-var port = parseInt(process.argv[2]); // TODO: null = 0
 var userDBPath = process.argv[3];
 var messagesDBPath = process.argv[4];
 
@@ -14,6 +26,8 @@ var messages = level(messagesDBPath);
 var Client = require('irc').Client;
 var client = new Client('open.ircnet.net', 'christianBNC');
 
-var server = require('../')(client, users, messages, port);
+var server = require('../')(client, users, messages, args.port);
 
-server.start();
+server.start(function () {
+    console.log('Service running on http://%s:%s', server.info.host, server.info.port);
+});
